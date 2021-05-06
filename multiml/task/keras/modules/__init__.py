532f5b1d75a939bbc52109a6cbde07a71ca5fb6d
Tomoe Kishimoto <tomoe@icepp.s.u-tokyo.ac.jp>
2021-05-06 20:42:32 +0900

    Add functional model

--- multiml/task/keras/modules/__init__.py
+++ multiml/task/keras/modules/__init__.py
@@ -1,4 +1,5 @@
 from .base_model import BaseModel
+from .functional_model import FunctionalModel
 from .softmax_dense_layer import SoftMaxDenseLayer
 from .mlp import MLPBlock
 from .conv2d import Conv2DBlock
@@ -7,6 +8,8 @@ from .darts_model import DARTSModel, SumTensor
 from .ensemble import EnsembleModel
 
 __all__ = [
+    'BaseModel',
+    'FunctionalModel',
     'SoftMaxDenseLayer',
     'MLPBlock',
     'Conv2DBlock',
