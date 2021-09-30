c82d03d2be5ce105b400418a8c78a2930ccbce0c
tomoe <tomoe@iutgpu01.iut.icepp.s.u-tokyo.ac.jp>
2021-09-30 17:58:02 +0900

    Add load_numpy method

--- multiml/data/data_util.py
+++ multiml/data/data_util.py
@@ -1,3 +1,4 @@
+import glob
 import numpy as np
 
 from multiml import logger
@@ -39,3 +40,30 @@ def load_boston(storegate, target_var_name='true', phase='train', shuffle=True):
     storegate.add_data(feature_names, data, phase=phase, shuffle=shuffle)
     storegate.add_data(target_var_name, target, phase=phase, shuffle=shuffle)
     storegate.compile()
+
+
+def load_numpy(storegate, file_path, var_names=None, phase='train', dtype=None, shuffle=True):
+    input_files = glob.glob(file_path)
+
+    for input_file in input_files:
+        logger.info(f'open {input_file}')
+
+        data = np.load(input_file)
+
+        if isinstance(phase, (tuple, list)):
+            if sum(phase) > 1.:
+                data = data[:sum(phase)]
+
+        if dtype is not None:
+            data = data.astype(dtype)
+
+        if var_names is None:
+            tmp_var_names = input_file.split('/')[-1]
+        else:
+            tmp_var_names = var_names
+
+        storegate.add_data(tmp_var_names, data, phase)
+
+    if shuffle:
+        storegate.compile()
+        storegate.shuffle()
