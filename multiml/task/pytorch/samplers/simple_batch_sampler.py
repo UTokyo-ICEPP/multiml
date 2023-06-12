24b0ba002ea349ccf26995d03e6359c25604474a
tomoe <tomoe@iutgpu01.iut.icepp.s.u-tokyo.ac.jp>
2023-06-12 15:41:10 +0900

    Update simple batch sampler

--- multiml/task/pytorch/samplers/simple_batch_sampler.py
+++ multiml/task/pytorch/samplers/simple_batch_sampler.py
@@ -1,10 +1,11 @@
 import torch
 from torch.utils.data import Sampler
+import numpy as np
 
 
 class SimpleBatchSampler(Sampler):
     def __init__(self, num_samples, batch_size, shuffle):
-        self.data = torch.arange(num_samples)
+        self.data = np.arange(num_samples)
         self.index = 0
         self.num_samples = num_samples
         self.batch_size = batch_size
@@ -12,7 +13,7 @@ class SimpleBatchSampler(Sampler):
 
     def __iter__(self):
         if self.shuffle:
-            self.data = torch.randperm(self.num_samples)
+            np.random.shuffle(self.data)
         self.index = 0
         return self
 
@@ -25,7 +26,7 @@ class SimpleBatchSampler(Sampler):
 
         self.index += 1
 
-        return self.data[index1:index2]
+        return self.data[index1:index2].tolist()
 
     def __len__(self):
         return -(-1 * self.num_samples // self.batch_size)
